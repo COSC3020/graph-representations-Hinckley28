0ad605541d98fc7a83fdf592069813a7cd4cb593
Hinckley28 <144728931+Hinckley28@users.noreply.github.com>
2024-03-12 15:35:11 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+
+    adjList = [];
+    for (let vert = 0; vert < adjMatrix.length; vert++) {
+        adjList[vert] = [];
+        for (let hori = 0; hori < adjMatrix[vert].length; hori++) {
+            if (adjMatrix[vert][hori] == 1) { 
+                adjList[vert].push(hori);
+               }
+        } 
+    }
+    return adjList;
 }
